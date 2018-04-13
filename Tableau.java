f64580d9be33b6c110f64c5b4bb05a5b54e21fed
RubenCardozo <cardozoruben@gmail.com>
2018-04-13 15:59:50 +0200

    Override Signed-off-by:RubenCardozo <cardozoruben@gmail.com>

--- Tableau.java
+++ Tableau.java
@@ -1,6 +1,9 @@
 
 package cours;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Tableau {
     public static void main(String[] args) {
         double[] td0 = {};
@@ -27,5 +30,43 @@ public class Tableau {
         Object to = new String[3];
         
         ((Object[])to)[0]="toto";
+        
+        System.out.println("**********************************");
+        double[] td3 = {10d, 2d, 8d, 4d, 5d, 6.0, 12d};
+        System.out.println(Arrays.binarySearch(td3,4d)); // pas trié error
+        System.out.println(Arrays.binarySearch(td3,3,6,4d));// trié trouvé
+        
+        Arrays.sort(td3);
+        System.out.println(Arrays.toString(td3));//
+        System.out.println(Arrays.binarySearch(td3,4d));//trié ok -> 2
+        System.out.println(Arrays.binarySearch(td3,7d));//trié ok -> -5 (-Position théorique)
+        
+        double[] td4= Arrays.copyOf(td3, td3.length*2);
+        double[] td5= Arrays.copyOfRange(td4, 4,9);
+        System.out.println(Arrays.toString(td4));
+        System.out.println("CopyOfRange "+Arrays.toString(td5));
+        System.arraycopy(td4,3,td3,0,4);
+        System.out.println(Arrays.toString(td3));
+        
+        Arrays.fill(td5, 10d);
+        double[] td6= Arrays.copyOf(td5,td5.length);
+        System.out.println(td5==td6);//false
+        System.out.println(td5.equals(td6));//false
+        System.out.println(Arrays.equals(td5, td6));//true
+        
+        Personne[] tp= new Personne[5];
+        tp[0] =new Personne(1, "AAAA");
+        tp[1] =new Personne(5, "CCCC");
+        tp[2] =new Personne(15,"BBBBB");
+        tp[3] =new Personne(10,"FFFFF");
+        tp[4] =new Personne(18,"EEEEE");
+        //System.out.println(tp[0].toString());
+        //System.out.println(String.valueOf(tp[0]));
+        
+        Arrays.sort(tp);
+        System.out.println(Arrays.toString(tp));
+        System.out.println(Arrays.binarySearch(tp, new Personne(10,"FFFFF")));
+        
+        Personne[] tp2= Arrays.copyOfRange(tp, 0, 5);
     }
 }
