352bb736ff2acc432d87ceb62ff3c73d6f18e09e
RubenCardozo <cardozoruben@gmail.com>
2018-04-13 16:34:22 +0200

    Override Signed-off-by:RubenCardozo <cardozoruben@gmail.com>

--- Tableau.java
+++ Tableau.java
@@ -68,5 +68,12 @@ public class Tableau {
         System.out.println(Arrays.binarySearch(tp, new Personne(10,"FFFFF")));
         
         Personne[] tp2= Arrays.copyOfRange(tp, 0, 5);
+        System.out.println(tp[0]==tp2[0]);//true
+        System.out.println(Arrays.equals(tp,tp2));//false
+        tp[4] =new Personne(18,"EEEEE");//false
+        System.out.println(Arrays.equals(tp,tp2));//false
+        System.out.println(tp[0]==tp2[0]);//false
+        System.out.println(tp[4].equals(tp2[4]));//false
+        
     }
 }
