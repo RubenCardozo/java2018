6fbd4f6fbd01baa8cb9230f17e30fc52ffa83e71
RubenCardozo <cardozoruben@gmail.com>
2018-04-19 09:35:38 +0200

    Exercise Solution Signed-off-by:RubenCardozo <cardozoruben@gmail.com>

--- Exer.java
+++ Exer.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cours;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -18,54 +16,71 @@ import java.util.Scanner;
 public class Exer {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Saisissez 'S'pour saisir , 'T' pour trier ou 'Q' pour sortir");
-        String str = sc.nextLine();
-        
-        ArrayList<LocalDate> ld = new ArrayList<LocalDate>();
-        DateTimeFormatter pat = DateTimeFormatter.ofPattern("d/M/yyyy");
-        
-        
+
+        System.out.println("Saisissez 'S'aisir ou 'Q'uiter");
+        String str = sc.nextLine().toUpperCase();
+
+        ArrayList<LocalDate> ld = new ArrayList<>();
+        DateTimeFormatter pat = DateTimeFormatter.ofPattern("d/M/yyyy", Locale.FRANCE);
+
         while (str.equals("S")) {
 
-            Scanner sc1 = new Scanner(System.in);
-            System.out.println("Saisissez une Date : ");
-            String str1 = sc1.nextLine();
+            saisirDate(sc, ld, pat);
             
-            TemporalAccessor ta = pat.parse(str1);
-
-            LocalDate date = LocalDate.from(ta);
-
-            System.out.println(date);
-
-            System.out.println("Vous venez de saisir la date: " + date.format(pat));
-
-            ld.add(date);
-
-           
-
-           
-            System.out.println("Saisissez 'S'pour saisir , 'T' pour trier ou 'Q' pour sortir");
-            str = sc.nextLine();
-            
-            
-            if (str.equals("L")) {
-                System.out.println("Liste de  dates: \n" + date.toString());
-                str = sc.nextLine();
-            }
-           
+            System.out.println("\nSaisissez: 'S'aisir, 'T'rier ou 'Q'uiter");
+            str = sc.nextLine().toUpperCase();
             
             if (str.equals("T")) {
-
+                
+                triDate(sc,str,ld);
+               
+            System.out.println("\nSaisissez: 'S'aisir , 'T'rier, 'Q'uiter");
+            str = sc.nextLine().toUpperCase();    
             }
-            
+
             if (str.equals("Q")) {
+                System.out.println("FIN!!!");
                 break;
             }
+            
+            
+
         }
+    }
 
-        //System.out.println("Saisissez 'S'pour saisir , 'T' pour trier ou 'Q' pour sortir");
+    static void saisirDate(Scanner sc, ArrayList<LocalDate> ld, DateTimeFormatter pat) {
+        System.out.println("Saisissez une Date (ex. 22/12/2000):");
+        String str1 = sc.nextLine();
 
+        TemporalAccessor ta = pat.parse(str1);
+
+        LocalDate date = LocalDate.from(ta);
+
+        ld.add(date);
+
+        System.out.println("La date: " + date + " ,vienne d'être ajoutée à la liste");
+    }
+
+    static void triDate(Scanner sc, String str, ArrayList<LocalDate> ld) {
+        System.out.println("\nChoisissez l'ordre: 'A'scendente ou 'D'escendente?");
+        str = sc.nextLine().toUpperCase();
+        if (str.equals("A")) {
+            ld.sort(new CompareDate());
+        }
+        if (str.equals("D")) {
+            ld.sort(new CompareDate().reversed());
+        }
+        System.out.println(ld);
+    }
+}
+
+class CompareDate implements Comparator<LocalDate> {
+
+    @Override
+    public int compare(LocalDate da1, LocalDate da2) {
+        return da1.compareTo(da2);
     }
 
 }
