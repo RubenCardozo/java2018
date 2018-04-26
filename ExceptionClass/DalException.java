1ecec4b467fd2e4c1a99fb4e0655664bc8d9e035
RubenCardozo <cardozoruben@gmail.com>
2018-04-26 13:56:52 +0200

    Les exceptions Signed-off-by:RubenCardozo <cardozoruben@gmail.com>

---
+++ ExceptionClass/DalException.java
@@ -0,0 +1,21 @@
+
+package cours.ExceptionClass;
+
+public class DalException extends Exception{
+
+    public DalException() {
+    }
+
+    public DalException(String message) {
+        
+    }
+
+    public DalException(String message, Throwable cause) {
+        
+    }
+
+    public DalException(Throwable cause) {
+        
+    }
+    
+}
---
+++ ExceptionClass/MetierException.java
@@ -0,0 +1,21 @@
+
+package cours.ExceptionClass;
+
+public class MetierException extends RuntimeException{
+
+    public MetierException() {
+    }
+
+    public MetierException(String message) {
+        
+    }
+
+    public MetierException(String message, Throwable cause) {
+        
+    }
+
+    public MetierException(Throwable cause) {
+        
+    }
+    
+}
